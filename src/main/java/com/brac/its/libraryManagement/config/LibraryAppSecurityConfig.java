ab51171d60aec7c7f649606448ccfe748ce98fb0
shakibul <shakibul.alam@bracits.com>
2022-06-21 16:35:00 +0600

    m-2 lesson 3 done

--- src/main/java/com/brac/its/libraryManagement/config/LibraryAppSecurityConfig.java
+++ src/main/java/com/brac/its/libraryManagement/config/LibraryAppSecurityConfig.java
@@ -2,10 +2,13 @@ package com.brac.its.libraryManagement.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class LibraryAppSecurityConfig {
+public class LibraryAppSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -14,5 +17,16 @@ public class LibraryAppSecurityConfig {
                 .withUser("user")
                 .password("{noop}pass")
                 .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/delete/**")
+                .hasAnyAuthority("ADMIN", "ADMIN2")
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
     }
 }
